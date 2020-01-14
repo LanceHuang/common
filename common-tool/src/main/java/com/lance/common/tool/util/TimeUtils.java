ba230c1c155764979b109b08ad1e8683219def30
634202275@qq.com <huanjmaxiada520>
2020-01-14 16:16:28 +0800

    Add Time utilities

---
+++ common-tool/src/main/java/com/lance/common/tool/util/TimeUtils.java
@@ -0,0 +1,15 @@
+package com.lance.common.tool.util;
+
+/**
+ * Utilities of time-format and so on.
+ *
+ * @author Lance
+ * @since 2019/6/18 15:58
+ */
+public class TimeUtils {
+
+    public static final long now() {
+        return System.currentTimeMillis();
+    }
+
+}
